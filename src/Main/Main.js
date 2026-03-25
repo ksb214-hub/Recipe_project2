b4d490c6b99632c98c98ea6f5fc176ef2f905f94
ksb214-hub <77158885+ksb214-hub@users.noreply.github.com>
2026-03-25 16:46:23 +0900

    ✔ main class=“con” ✔ div id=“tab” ✔ ul → li 구조 ✔ 첫 번째 li class=“on” ✔ “레시피 / 냉장고 재료” 탭 구현 ✔ flex 적용

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,4 +1,5 @@
 // src/Main/Main.js
+// src/Main/Main.js
 import React from "react";
 import "./Main.css";
 import { Link } from "react-router-dom";
@@ -6,31 +7,48 @@ import { Link } from "react-router-dom";
 function Main() {
   return (
     <div>
-      {/* 🔹 NAV */}
+      {/* NAV */}
       <nav className="zero-naeng-fe_nav">
         <ul>
-            {/* 로고 */}
-            <li>
-                <Link className="zero-naeng-fe_link" to="/">
-                zero-naeng-fe
-                </Link>
-            </li>
+          {/* 로고 */}
+          <li>
+            <Link className="zero-naeng-fe_link" to="/">
+              zero-naeng-fe
+            </Link>
+          </li>
 
-            {/* 로그인 */}
-            <li>
-                <Link className="zero-naeng-fe_login_btn" to="/login">
-                로그인
-                </Link>
-            </li>
+          {/* 로그인 */}
+          <li>
+            <Link className="zero-naeng-fe_login_btn" to="/login">
+              로그인
+            </Link>
+          </li>
 
-            {/* 검색 */}
-            <li>
-                <Link className="zero-naeng-fe_search" to="/search">
-                검색
-                </Link>
-            </li>
+          {/* 검색 */}
+          <li>
+            <Link className="zero-naeng-fe_search" to="/search">
+              검색
+            </Link>
+          </li>
         </ul>
       </nav>
+
+      {/* MAIN */}
+      <main className="con">
+        <div id="tab">
+          <ul>
+            {/* 레시피 */}
+            <li className="on">
+              <a href="#">레시피</a>
+            </li>
+
+            {/* 냉장고 재료 */}
+            <li>
+              <a href="#">냉장고 재료</a>
+            </li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
